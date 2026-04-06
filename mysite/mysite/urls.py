fbb70e94a1484779d9fd8fbb6ca1acd8b70938b6
jadeambos123 <ambos.jade123@gmail.com>
2026-04-07 01:15:12 +0800

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from django.http import JsonResponse
 from django.views.static import serve
 from grocery import views as grocery_views
-from .views import CustomAuthToken
 
 def home(request):
     return JsonResponse({
