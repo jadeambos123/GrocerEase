640dfa2372ccb4049731550dd9c85c56ae070df9
jadeambos123 <ambos.jade123@gmail.com>
2026-04-07 12:37:46 +0800

    Update serializers.py

--- mysite/grocery/serializers.py
+++ mysite/grocery/serializers.py
@@ -1,3 +1,8 @@
+from django.core.files.base import ContentFile
+from urllib.parse import urlparse
+import os
+import urllib.request
+
 from rest_framework import serializers
 from .models import Product, Category, Cart, Order, OrderItem
 
@@ -16,17 +21,48 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     category_name = serializers.ReadOnlyField(source='category.name')
-    image = serializers.SerializerMethodField()
+    image = serializers.ImageField(required=False, allow_null=True)
+    image_url = serializers.SerializerMethodField(read_only=True)
+    remote_image_url = serializers.URLField(write_only=True, required=False, allow_blank=True)
+    unit = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'category', 'category_name', 'description', 'price', 'stock', 'image']
+        fields = ['id', 'name', 'category', 'category_name', 'description', 'price', 'stock', 'image', 'image_url', 'remote_image_url', 'unit']
 
-    def get_image(self, obj):
+    def get_image_url(self, obj):
         if obj.image:
             request = self.context.get('request')
             return request.build_absolute_uri(obj.image.url) if request else obj.image.url
         return None
+
+    def _download_remote_image(self, url):
+        try:
+            response = urllib.request.urlopen(url)
+            content = response.read()
+            parsed = urlparse(url)
+            name = os.path.basename(parsed.path) or 'remote_image'
+            if not os.path.splitext(name)[1]:
+                name += '.jpg'
+            return ContentFile(content, name=name)
+        except Exception as exc:
+            raise serializers.ValidationError({'remote_image_url': f'Could not download image: {exc}'})
+
+    def create(self, validated_data):
+        remote_url = validated_data.pop('remote_image_url', None)
+        image = validated_data.get('image')
+        if remote_url and not image:
+            validated_data['image'] = self._download_remote_image(remote_url)
+        validated_data.pop('unit', None)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        remote_url = validated_data.pop('remote_image_url', None)
+        image = validated_data.get('image')
+        if remote_url and not image:
+            validated_data['image'] = self._download_remote_image(remote_url)
+        validated_data.pop('unit', None)
+        return super().update(instance, validated_data)
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product_image = serializers.SerializerMethodField()
